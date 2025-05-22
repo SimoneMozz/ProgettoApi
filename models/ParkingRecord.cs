635dd0c4db8fdb8a19b443f21a90eaa2e357c3ed
user <mozzillosimone0@gmail.com>
2025-05-22 17:42:28 +0200

    tutto

---
+++ models/ParkingRecord.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgettoApi.models
+{
+    public class ParkingRecord
+    {
+        public Guid Id { get; set; }
+        public string Plate { get; set; }
+        public DateTime EntryTime { get; set; }
+    }
+}
