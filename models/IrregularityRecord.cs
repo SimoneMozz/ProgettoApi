d09e3ce8c5911f63d502286a5f6a58885e352624
Ranfagni <Ranfagni@LAPTOP-0QNR8SIC>
2025-05-28 12:31:57 +0200

    coversione da List a Db

--- models/IrregularityRecord.cs
+++ models/IrregularityRecord.cs
@@ -1,9 +1,14 @@
-﻿namespace ProgettoApi.models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgettoApi.models
 {
     public class IrregularityRecord
     {
+        [Key]
+        public int Id { get; set; }
         public string Plate { get; set; }
         public DateTime Date { get; set; }
         public int Count { get; set; }
     }
 }
+
--- models/ParkingRecord.cs
+++ models/ParkingRecord.cs
@@ -1,9 +1,27 @@
-﻿namespace ProgettoApi.models
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProgettoApi.models
 {
+    [Table("ParkingRecords")]
     public class ParkingRecord
     {
+        [Key]
         public Guid TicketId { get; set; }
         public string Plate { get; set; }
         public DateTime EntryTime { get; set; }
+    }
+
+    public class ParkingRecordConfiguration : IEntityTypeConfiguration<ParkingRecord>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<ParkingRecord> builder)
+        {
+            builder.HasKey(pr => pr.TicketId);
+            builder.Property(pr => pr.Plate).IsRequired().HasMaxLength(20);
+            builder.Property(pr => pr.EntryTime).IsRequired();
+        }
     }
 }
